55ebe479a00a492c85ec5307c66617b1d0fc9200
Adriano Brito dos Santos <adriano.bsanto@eng.ci.ufpb.br>
2020-09-04 15:20:34 -0300

    CRUD + Middlewares

--- modulo01/index.js
+++ modulo01/index.js
@@ -2,14 +2,73 @@ const express = require("express");
 
 const server = express();
 
+server.use(express.json());
+
 //request body = {"name":"adriano","age":23}
+//CRUD => CREATE, READER,UPDATE,DELETE
 
-server.get("/users/:id", (req, res) => {
-  const nome = req.query.nome; //query params = ?teste=1
+const users = ["Adriano", "Mari", "João"];
 
-  const id = req.params.id; //route params = /users/1
+server.use((req, res, next) => {
+  console.time("Request");
+  console.log(`Método : ${req.method}, URL:${req.url}`);
 
-  return res.send({ message: `Hello ${nome}`, user: id });
+  next();
+
+  console.timeEnd("Request");
 });
 
+const checkNameExists = (req, res, next) => {
+  if (!req.body.name) {
+    return res
+      .status(400)
+      .json({ error: "param `name` not found on request body" });
+  }
+  return next();
+};
+
+const checkUserInArray = (req, res, next) => {
+  const user = users[req.params.index];
+  if (!user) {
+    return res.status(400).json({ error: "User does not exist" });
+  }
+  req.user = user;
+  return next();
+};
+
+server.get("/users", (req, res) => {
+  return res.json(users);
+});
+
+server.get("/users/:index", checkUserInArray, (req, res) => {
+  const name = req.query.name; //query params = ?teste=1
+
+  return res.json(req.user);
+});
+
+server.post("/users/", checkNameExists, (req, res) => {
+  // const name = req.body.name;
+  const { name } = req.body;
+
+  users.push(name);
+
+  return res.json(users);
+});
+
+server.put("/users/:index", checkNameExists, checkUserInArray, (req, res) => {
+  const { index } = req.params;
+  const { name } = req.body;
+
+  users[index] = name;
+
+  return res.json(users);
+});
+
+server.delete("/users/:index", checkUserInArray, (req, res) => {
+  const { index } = req.params;
+
+  users.splice(index, 1);
+
+  return res.send();
+});
 server.listen(3000);
